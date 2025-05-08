a0f8afac38624bcaeb70c2fb09e8b9468e42088a
Inesh De Zoysa <121690731+Enstorm5@users.noreply.github.com>
2025-05-08 19:30:34 +0530

    Update search.js

--- search.js
+++ search.js
@@ -1,7 +1,7 @@
 // search.js
 
-const publicKey = '921b7bd5cc447b7c1afd5948b2ddedab';
-const privateKey = '38edf8f12481fe6b68193462848a40fac84471b3';
+const publicKey = '';
+const privateKey = '';
 const apiUrl = 'https://gateway.marvel.com/v1/public/comics';
 const ts = 1;
 const hash = 'ae7cc04b72f12dc607f1e02867b7c6a2';
